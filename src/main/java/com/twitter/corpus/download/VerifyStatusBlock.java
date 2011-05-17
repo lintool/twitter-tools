58742550a0e31b0e103fe5ee08f775396b92d4f0
lintool <jimmy@twitter.com>
2011-05-16 20:01:07 -0400

    Refactored VerifyStatusBlock... seems like a reasonable implementation for now.

--- src/main/java/com/twitter/corpus/download/VerifyStatusBlock.java
+++ src/main/java/com/twitter/corpus/download/VerifyStatusBlock.java
@@ -5,10 +5,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.zip.GZIPOutputStream;
 
 import org.apache.commons.cli.CommandLine;
@@ -24,19 +26,176 @@ import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.Response;
 import com.twitter.corpus.data.Status;
 import com.twitter.corpus.data.StatusBlockReader;
-import com.twitter.corpus.data.StatusCorpusReader;
 import com.twitter.corpus.data.StatusStream;
 
 public class VerifyStatusBlock {
   private static final Logger LOG = Logger.getLogger(VerifyStatusBlock.class);
 
-  private VerifyStatusBlock() {}
+  private final File data;
+  private final File statuses;
+  private final AsyncHttpClient client = new AsyncHttpClient();
+
+  private File outputSuccess = null;
+  private File outputFailure = null;
+  private File repairedOutput = null;
+
+  public VerifyStatusBlock(File data, File statuses) {
+    this.statuses = statuses;
+    if (!statuses.exists()) {
+      throw new RuntimeException(statuses + " does not exist!");
+    }
+
+    this.data = data;
+  }
+
+  public VerifyStatusBlock withOutputSuccess(File file) {
+    this.outputSuccess = file;
+    return this;
+  }
+
+  public VerifyStatusBlock withOutputFailure(File file) {
+    this.outputFailure = file;
+    return this;
+  }
+
+  public VerifyStatusBlock withRepairedOutput(File file) {
+    this.repairedOutput = file;
+    return this;
+  }
+
+  public boolean verify() throws IOException {
+    LOG.info(String.format("Reading statuses read from %s.", statuses));
+
+    StatusStream stream;
+    if (statuses.isDirectory()) {
+      throw new RuntimeException(statuses + " cannot be a directory!");
+    }
+    stream = new StatusBlockReader(statuses);
+
+    Map<Long, String> ids = new HashMap<Long, String>();
+
+    int cnt = 0;
+    Status status;
+    while ((status = stream.next()) != null) {
+      ids.put(status.getId(), status.getJsonString());
+      cnt++;
+    }
+    LOG.info(String.format("Total of %d statuses read.", cnt));
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(data)));
+
+    FileWriter successOut = null;
+    if (outputSuccess != null) {
+      successOut = new FileWriter(outputSuccess);
+    }
+    FileWriter failureOut = null;
+    if (outputFailure != null) {
+      failureOut = new FileWriter(outputFailure);
+    }
+
+    int totalCnt = 0;
+    int successCnt = 0;
+    int failureCnt = 0;
+    int fetchedCnt = 0;
+    int notAvailableCnt = 0;
+    String line;
+    while ((line = in.readLine()) != null) {
+      String[] arr = line.split("\\t");
+      long id = Long.parseLong(arr[0]);
+      totalCnt++;
+
+      if (ids.containsKey(id)) {
+        if (successOut != null) {
+          successOut.write(line + "\n");
+        }
+        successCnt++;
+      } else {
+        Response response = null;
+        while (true) {
+          try {
+            response = client.prepareGet(
+                AsyncStatusBlockFetcher.getUrl(AsyncStatusBlockFetcher.DEFAULT_URL_PREFIX, id, arr[1]))
+                .execute().get();
+
+            if (response.getStatusCode() < 500) {
+              break;
+            }
+          } catch (InterruptedException e) {
+            // Do nothing, just retry.
+          } catch (ExecutionException e) {
+            // Do nothing, just retry.
+          }
+
+          try {
+            Thread.sleep(1000);
+          } catch (Exception e) {}
+          LOG.warn("Error: retrying.");
+        }
+
+        String s = response.getResponseBody();
+        if (isTweetNoLongerAvailable(s)) {
+          LOG.info(String.format("Missing status %d: no longer available.", id));
+          notAvailableCnt++;
+        } else {
+          LOG.info(String.format("Missing status %d: successfully fetched.", id));
+          ids.put(id, response.getResponseBody());
+          fetchedCnt++;
+        }
+
+        if (failureOut != null) {
+          failureOut.write(line + "\n");
+        }
+        failureCnt++;
+      }
+    }
+
+    LOG.info(String.format("Total of %d statuses in %s.", cnt, statuses));
+    LOG.info(String.format("Total of %d entries in %s.", totalCnt, data));
+    LOG.info(String.format("%d statuses no longer available.", notAvailableCnt));
+    LOG.info(String.format("%d missing statuses fetched.", fetchedCnt));
+
+    if (cnt + notAvailableCnt + fetchedCnt == totalCnt) {
+      LOG.info("SUCCESS! All statuses accounted for.");
+    }
+
+    if (outputSuccess != null) {
+      LOG.info(String.format("Total of %d status id written to %s.", successCnt, outputSuccess));
+      successOut.close();
+    }
+    if (outputFailure != null) {
+      LOG.info(String.format("Total of %d status id written to %s", failureCnt, outputFailure));
+      failureOut.close();
+    }
+
+    in.close();
+    client.close();
+
+    if (repairedOutput != null) {
+      LOG.info("Writing tweets...");
+      int written = 0;
+      OutputStreamWriter out = new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(repairedOutput)));
+      for (Map.Entry<Long, String> entry : ids.entrySet()) {
+        written++;
+        out.write(entry.getValue() + "\n");
+      }
+      out.close();
+      LOG.info(written + " statuses written.");
+      LOG.info("Done!");
+    }
+
+    return true;
+  }
+
+  public static boolean isTweetNoLongerAvailable(String s) {
+    return s.contains("Sorry, you are not authorized to see this status.") ||
+        s.contains("No status found with that ID.");
+  }
 
   private static final String STATUSES_OPTION = "statuses";
   private static final String STATUSES_REPAIRED_OPTION = "statuses_repaired";
   private static final String DATA_OPTION = "data";
-  private static final String SUCCESS_OPTION = "output_success";
-  private static final String FAILURE_OPTION = "output_failure";
+  private static final String OUTPUT_SUCCESS_OPTION = "output_success";
+  private static final String OUTPUT_FAILURE_OPTION = "output_failure";
 
   @SuppressWarnings("static-access")
   public static void main(String[] args) throws Exception {
@@ -50,9 +209,9 @@ public class VerifyStatusBlock {
     options.addOption(OptionBuilder.withArgName("path").hasArg()
         .withDescription("data file with tweet ids").create(DATA_OPTION));
     options.addOption(OptionBuilder.withArgName("path").hasArg()
-        .withDescription("output file for tweet fetch successes").create(SUCCESS_OPTION));
+        .withDescription("output file for tweet fetch successes").create(OUTPUT_SUCCESS_OPTION));
     options.addOption(OptionBuilder.withArgName("path").hasArg()
-        .withDescription("output file for tweet fetch failures").create(FAILURE_OPTION));
+        .withDescription("output file for tweet fetch failures").create(OUTPUT_FAILURE_OPTION));
 
     CommandLine cmdline = null;
     CommandLineParser parser = new GnuParser();
@@ -63,114 +222,27 @@ public class VerifyStatusBlock {
       System.exit(-1);
     }
 
-    if (!cmdline.hasOption(STATUSES_OPTION) || !cmdline.hasOption(DATA_OPTION) ||
-        !cmdline.hasOption(SUCCESS_OPTION) | !cmdline.hasOption(FAILURE_OPTION) ||
-        !cmdline.hasOption(STATUSES_REPAIRED_OPTION) ) {
+    if (!cmdline.hasOption(STATUSES_OPTION) || !cmdline.hasOption(DATA_OPTION)) {
       HelpFormatter formatter = new HelpFormatter();
       formatter.printHelp(VerifyStatusBlock.class.getName(), options);
       System.exit(-1);
     }
 
-    AsyncHttpClient client = new AsyncHttpClient();
+    VerifyStatusBlock v = new VerifyStatusBlock(new File(cmdline.getOptionValue(DATA_OPTION)),
+        new File(cmdline.getOptionValue(STATUSES_OPTION)));
 
-    File file = new File(cmdline.getOptionValue(STATUSES_OPTION));
-    if (!file.exists()) {
-      System.err.println("Error: " + file + " does not exist!");
-      System.exit(-1);
+    if (cmdline.hasOption(OUTPUT_SUCCESS_OPTION)) {
+      v.withOutputSuccess(new File(cmdline.getOptionValue(OUTPUT_SUCCESS_OPTION)));
     }
 
-    LOG.info(String.format("Reading statuses read from %s.", file));
-
-    StatusStream stream;
-    if (file.isDirectory()) {
-      stream = new StatusCorpusReader(file);
-    } else {
-      stream = new StatusBlockReader(file);
+    if (cmdline.hasOption(OUTPUT_FAILURE_OPTION)) {
+      v.withOutputFailure(new File(cmdline.getOptionValue(OUTPUT_FAILURE_OPTION)));
     }
 
-    Map<Long, String> ids = new HashMap<Long, String>();
-
-    int cnt = 0;
-    Status status;
-    while ((status = stream.next()) != null) {
-      ids.put(status.getId(), status.getJsonString());
-      cnt++;
-    }
-    LOG.info(String.format("Total of %d statuses read.", cnt, file));
-
-    BufferedReader data = new BufferedReader(new InputStreamReader(
-        new FileInputStream(cmdline.getOptionValue(DATA_OPTION))));
-
-    FileWriter successOut = new FileWriter(new File(cmdline.getOptionValue(SUCCESS_OPTION)));
-    FileWriter failureOut = new FileWriter(new File(cmdline.getOptionValue(FAILURE_OPTION)));
-
-    int successCnt = 0;
-    int failureCnt = 0;
-    String line;
-    while ((line = data.readLine()) != null) {
-      String[] arr = line.split("\\t");
-      long id = Long.parseLong(arr[0]);
-      if (ids.containsKey(id)) {
-        successOut.write(line + "\n");
-        successCnt++;
-      } else {
-        LOG.warn(String.format("Missing tweet id %d", id));
-
-        Response response;
-        while (true) {
-          response = client.prepareGet(
-              AsyncStatusBlockFetcher.getUrl(AsyncStatusBlockFetcher.DEFAULT_URL_PREFIX, id, arr[1]))
-              .execute().get();
-          if (response.getStatusCode() < 500) {
-            break;
-          }
-
-          try {
-            Thread.sleep(1000);
-          } catch (Exception e) {}
-          LOG.warn("Error: retrying.");
-        }
-
-        String s = response.getResponseBody();
-        if (isTweetNoLongerAvailable(s)) {
-          LOG.info(String.format("Tweet id %d is no longer available. Status OKAY.", id));
-        } else {
-          LOG.info(String.format("Successfully fetched tweet id %d", id));
-          ids.put(id, response.getResponseBody());
-          //LOG.info(response.getResponseBody());
-        }
-
-        failureOut.write(line + "\n");
-        failureCnt++;
-      }
+    if (cmdline.hasOption(STATUSES_REPAIRED_OPTION)) {
+      v.withRepairedOutput(new File(cmdline.getOptionValue(STATUSES_REPAIRED_OPTION)));
     }
 
-    LOG.info(String.format("Total of %d status id written to %s.",
-        successCnt, cmdline.getOptionValue(SUCCESS_OPTION)));
-    LOG.info(String.format("Total of %d status id written to %s",
-        failureCnt, cmdline.getOptionValue(FAILURE_OPTION)));
-
-    successOut.close();
-    failureOut.close();
-    data.close();
-    client.close();
-
-    LOG.info("Writing tweets...");
-    String output = cmdline.getOptionValue(STATUSES_REPAIRED_OPTION);
-    int written = 0;
-    OutputStreamWriter out =
-      new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(output)));
-    for ( Map.Entry<Long, String> entry : ids.entrySet()) {
-      written++;
-      out.write(entry.getValue() + "\n");
-    }
-    out.close();
-    LOG.info(written + " statuses written.");
-    LOG.info("Done!");
-  }
-
-  public static boolean isTweetNoLongerAvailable(String s) {
-    return s.contains("Sorry, you are not authorized to see this status.") ||
-        s.contains("No status found with that ID.");
+    v.verify();
   }
 }
