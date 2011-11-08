36ab27acd4bde6429fea66e26ffe3e2af903bead
Diego Caro <diegocaro@gmail.com>
2011-11-08 18:09:46 -0300

    Adding a new test case for location features.

--- src/test/java/com/twitter/corpus/download/FetchStatusTest.java
+++ src/test/java/com/twitter/corpus/download/FetchStatusTest.java
@@ -32,6 +32,31 @@ public class FetchStatusTest {
 
     asyncHttpClient.close();
   }
+  
+  @Test
+  public void anotherFamous() throws Exception {
+    String url = AsyncHtmlStatusBlockCrawler.getUrl(132336185812070401L, "bs");
+    AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+    AsyncHttpClient.BoundRequestBuilder request = asyncHttpClient.prepareGet(url);
+    Future<Response> f = request.execute();
+    Response response = f.get();
+
+    // Make sure status is OK.
+    assertEquals(200, response.getStatusCode());
+    String html = response.getResponseBody("UTF-8");
+
+    Status status = Status.fromHtml(132336185812070401L,"bs", 200, html);
+    assertEquals(132336185812070401L, status.getId());
+    assertEquals("bs", status.getScreenname());
+    assertEquals("@olofster, wish you were here!", status.getText());
+    assertEquals("132332780720898049", status.getReplyOf());
+    assertEquals("Centro, Tabasco", status.getLocation());
+    assertEquals("a0536fb1dd01b8d5", status.getPlaceId());
+    assertEquals("17.97813796", status.getLatitude());
+    assertEquals("-92.93089606", status.getLongitude());
+    assertEquals(1320386380, status.getTimestamp());
+    asyncHttpClient.close();
+  }
 
   public static junit.framework.Test suite() {
     return new JUnit4TestAdapter(FetchStatusTest.class);
