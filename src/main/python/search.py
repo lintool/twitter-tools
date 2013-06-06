f7e04b31c9f92b1161eaa82d23983f87cda52a3a
James McMinn <a.mcminn.1@research.gla.ac.uk>
2013-06-06 15:29:41 +0100

    Added Python thrift bindings and some example code

--- src/main/python/search.py
+++ src/main/python/search.py
@@ -1,13 +1,12 @@
 import sys
 
 # Path the the automatically generated thrift files
-# Alternativly, the files in the /src/gen/python directory can be added to your python path
+# Alternatively, the files in the /src/gen/python directory can be added to your python path
 sys.path.append('./gen/python')
 
 from Search import TrecSearch, ttypes
 
 import argparse
-import xml.etree.ElementTree as ET
 
 from thrift import Thrift
 from thrift.transport import TSocket
@@ -15,18 +14,18 @@ from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
 try:
+    # Command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-host', dest="host", help='server to connect to', required=True)
     parser.add_argument('-port',type=int, dest="port", help='port to use', required=True)
-    parser.add_argument('-qid', dest="qid", help='query id', required=True)
-    parser.add_argument('-q', dest="query", help='query text', required=True)
-    parser.add_argument('-runtag', dest="run_tag", help='runtag', required=True)
-    parser.add_argument('-max_id', dest="max_id", help='maxid', required=True)
-    parser.add_argument('-num_results', dest="num_results", help='number of results', required=True)
     parser.add_argument('-group', dest="group", help='group id', required=True)
     parser.add_argument('-token', dest="token", help='access token', required=True)
+    parser.add_argument('-qid', dest="qid", help='query id', required=False, default='MB01')
+    parser.add_argument('-q', dest="query", help='query text', required=False, default='BBC World Service staff cuts')
+    parser.add_argument('-runtag', dest="run_tag", help='runtag', required=False, default='lucene4lm')
+    parser.add_argument('-max_id', dest="max_id", help='maxid', required=False, default=34952194402811905)
+    parser.add_argument('-num_results', dest="num_results", help='number of results', required=False, default=10)
     args = parser.parse_args()
-
 
     # Init thrift connection and protocol handlers
     transport = TSocket.TSocket(args.host, args.port)
@@ -37,27 +36,23 @@ try:
     # Open the connection to the server
     transport.open()
 
+    # Create a new query
     q = ttypes.TQuery()
     q.group = args.group
     q.token = args.token
     q.text = args.query
     q.max_id = long(args.max_id)
-    q.num_results = long(args.num_results)
+    q.num_results = int(args.num_results)
 
-    print args.max_id
-    print int(args.max_id)
-
-    # Performs the actual search on index
+    # Performs the actual search on the index
     results = client.search(q)
 
-    i = 1
-    for result in results:
+    for i, result in enumerate(results, 1):
         # TREC_eval formatted line
         print "%s Q0 %d %d %f %s" % (args.qid, result.id, i, result.rsv, args.run_tag)
-        i += 1
 
     # Close connection
     transport.close()
 
 except Thrift.TException, tx:
-    print 'Something went wrong : %s' % (tx.message)
+    print 'Thrift TException: %s' % (tx.message)
