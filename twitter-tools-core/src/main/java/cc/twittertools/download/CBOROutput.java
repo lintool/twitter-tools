300f052c4f87477748a16914b5613f5aba048ec6
Ian Soboroff <isoboroff@gmail.com>
2015-06-01 09:33:58 -0600

    Fixed several bugs: read the JSON object with a JSON parser, not a CBOR parser; CBOR needs a byte target, so we need our own open/close; remind Jackson not to close the stream for us.
    Confirmed working on a set of 20 tweet ids, reading the output with
    other CBOR tools.

--- twitter-tools-core/src/main/java/cc/twittertools/download/CBOROutput.java
+++ twitter-tools-core/src/main/java/cc/twittertools/download/CBOROutput.java
@@ -1,32 +1,54 @@
 package cc.twittertools.download;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Map;
+import java.util.zip.GZIPOutputStream;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
 
 public class CBOROutput extends GZipJSONOutput {
 
-  CBORFactory fac;
-  ObjectMapper mapper;
+  CBORFactory cbor_fac;
+  ObjectMapper cbor_mapper;
+  JsonFactory json_fac;
+  ObjectMapper json_mapper;
+  OutputStream os;
   
   public CBOROutput(File output_file) {
     super(output_file);
-    fac = new CBORFactory();
-    mapper = new ObjectMapper(fac);
+    cbor_fac = new CBORFactory();
+    cbor_fac.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+    cbor_mapper = new ObjectMapper(cbor_fac);
+    json_fac = new JsonFactory();
+    json_mapper = new ObjectMapper(json_fac);
   }
 
-  @SuppressWarnings("unchecked")
+  @Override
+  public void open() throws Exception {
+    os = new GZIPOutputStream(new FileOutputStream(output_file));
+  }
+
+  @Override @SuppressWarnings("unchecked")
   public void write(String s) throws Exception {
     Map<String, Object> obj;
     try {
-      obj = mapper.readValue(s, Map.class);
+      obj = json_mapper.readValue(s, Map.class);
     } catch (Exception e) {
       e.printStackTrace();
       return;
     }
-    mapper.writeValue(out, obj);
+    cbor_mapper.writeValue(os, obj);
   }
   
+  @Override
+  public void close() throws Exception {
+    os.close();
+  }
 }
